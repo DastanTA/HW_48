ce2fb8eb0ba541cae99189c9daba78a6f8ff1508
Dastan T <dastan.turatbekov@gmail.com>
2022-11-13 12:49:33 +0600

    Этап-1. Создал супер пользователя. Зарегистрировал модель в админке. Добавил данные для модели. Создал фикстуру.

--- source/ebay/admin.py
+++ source/ebay/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from ebay.models import Product
 
-# Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'remainder', 'price']
+    list_filter = ['category']
+    search_fields = ['name']
+    readonly_fields = []
+    exclude = []
+
+
+admin.site.register(Product, ProductsAdmin)
--- source/ebay/views.py
+++ source/ebay/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from ebay.models import Product, CATEGORY_CHOICES
 
 # Create your views here.

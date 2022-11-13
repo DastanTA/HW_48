c13e8bfbac50da30d5a45b350c9c14a1a6901077
Dastan T <dastan.turatbekov@gmail.com>
2022-11-13 17:21:02 +0600

    Этап-2. Реализован детальный просмотр товара. 2-этап выполнен пока без бонуса

--- source/ebay/views.py
+++ source/ebay/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from ebay.models import Product, CATEGORY_CHOICES
+from django.shortcuts import render, get_object_or_404
+from ebay.models import Product
+
 
 def main_page(request):
     products = Product.objects.all().order_by("category", "name").filter(remainder__gt=0)
     context = {'products': products}
     return render(request, 'index.html', context)
+
+
+def view_product(request, pk, *args, **kwargs):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product.html', {'product': product})
--- source/main/urls.py
+++ source/main/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from ebay.views import main_page
+from ebay.views import main_page, view_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page, name='main'),
+    path('item/<int:pk>', view_product, name='view_product'),
 ]

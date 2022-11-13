927e555ef8d6eac81ae6ed9fa2f042effc457e80
Dastan T <dastan.turatbekov@gmail.com>
2022-11-13 16:58:10 +0600

    Этап-2. Вывод на главную страницу выполнен. Но сортировка идет не по значению а по коду категорий. Пока не решил это

--- source/ebay/views.py
+++ source/ebay/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from ebay.models import Product, CATEGORY_CHOICES
 
-# Create your views here.
+def main_page(request):
+    products = Product.objects.all().order_by("category", "name").filter(remainder__gt=0)
+    context = {'products': products}
+    return render(request, 'index.html', context)
--- source/main/urls.py
+++ source/main/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from ebay.views import main_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page, name='main'),
 ]

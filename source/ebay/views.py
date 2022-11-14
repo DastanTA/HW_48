d5f1abfde2c6ea4ce25b50fff310f7c820fbd030
Dastan T <dastan.turatbekov@gmail.com>
2022-11-14 22:38:20 +0600

    Этап-3. Представление для добавления нового продукта добавлена. +Шаблон для добавления тоже добавлен. Добавлен шаблон для include

--- source/ebay/views.py
+++ source/ebay/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from ebay.models import Product
+from ebay.forms import ProductForm
 
 
 def main_page(request):
@@ -13,8 +14,23 @@ def view_product(request, pk, *args, **kwargs):
     return render(request, 'product.html', {'product': product})
 
 
-def create_product(request, pk, *args, **kwargs):
-    pass
+def create_product(request, *args, **kwargs):
+    if request.method == "GET":
+        form = ProductForm()
+        return render(request, 'create.html', {'form': form})
+    elif request.method == "POST":
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            Product.objects.create(
+                name=form.cleaned_data.get('name'),
+                category=form.cleaned_data.get('category'),
+                price=form.cleaned_data.get('price'),
+                remainder=form.cleaned_data.get('remainder'),
+                description=form.cleaned_data.get('description')
+            )
+            return redirect('main')
+        else:
+            return render(request, 'create.html', {'form': form})
 
 
 def update_product(request, pk, *args, **kwargs):
